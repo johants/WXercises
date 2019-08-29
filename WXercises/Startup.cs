47deb5a7de6da07845d64e3f1cc6e575c1bb93f1
Johan Sugiarto <johan.tjahyadi@gmail.com>
2019-08-30 09:31:59 +1000

    Initial commit

---
+++ WXercises/Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace WXercises
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("doc",
+                    new Info
+                    {
+                        Title = typeof(Program).Assembly.GetName().Name,
+                        Version = typeof(Program).Assembly.GetName().Version.ToString()
+                    });
+            });
+
+            // JSON settings
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Converters = new List<JsonConverter> { new StringEnumConverter() }
+            };
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app
+                .UseSwagger()
+                .UseSwaggerUI(options =>
+                    {
+                        options.SwaggerEndpoint("doc/swagger.json", GetType().GetTypeInfo().Assembly.GetName().Name);
+                    });
+            
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
